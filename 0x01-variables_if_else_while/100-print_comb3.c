dd6c21cf4695a9707bdc07f1dfcbfb34cb901942
n.bakenchich@gmail.com <n.bakenchich@gmail.com>
2023-03-17 08:56:09 -0700

    task 10 => combinations

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,10 +14,10 @@ for (i = 0; i < 9; i++)
 {
 for (j = i + 1; j < 10; j++)
 {
-if (i == 8 && j == 9)
-continue;
 putchar((i % 10) + '0');
 putchar((j % 10) + '0');
+if (i == 8 && j == 9)
+continue;
 putchar(',');
 putchar(' ');
 }
