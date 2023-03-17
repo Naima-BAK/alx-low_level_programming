1b65634d49b6f72c5163a0cb142866dcf79a7690
n.bakenchich@gmail.com <n.bakenchich@gmail.com>
2023-03-17 07:57:01 -0700

    task 5 => numbers in base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *  main - prints exactly numbers in base 10,
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n;
+
+for (n = 0; n < 10; n++)
+{
+printf("%d", n);
+}
+printf("\n");
+return (0);
+}
