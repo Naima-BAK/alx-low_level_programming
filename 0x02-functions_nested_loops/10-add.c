9089fd88afd695966156229a559340efbb1c753c
n.bakenchich@gmail.com <n.bakenchich@gmail.com>
2023-03-21 17:27:40 -0700

    task 10 => adds two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - adds two integers
+ * @num2: number 1
+ * @num1: number 2
+ * Return: sum.
+ */
+int add(int num1, int num2)
+{
+int sum = num1 + num2;
+return (sum);
+}
