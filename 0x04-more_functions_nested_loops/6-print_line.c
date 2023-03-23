bdb65dd072b05084e8d8c89a51b6185be105ad0e
n.bakenchich@gmail.com <n.bakenchich@gmail.com>
2023-03-23 15:08:10 -0700

    task 6 => draws a straight line in the terminal

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -2,7 +2,7 @@
 
 /**
 * print_line - Write a function that draws a straight line in the terminal.
-* @n: integer
+* @n: the number of times the character _ should be printed
 **/
 void print_line(int n)
 {
