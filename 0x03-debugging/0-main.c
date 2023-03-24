7d02e6929978877782e32dd858d11848c90120d2
n.bakenchich@gmail.com <n.bakenchich@gmail.com>
2023-03-23 19:22:23 -0700

    task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,13 +1,16 @@
 #include "main.h"
 
 /**
- * main - test - positive or negative
- * Return: Always 0
+ * main - test the function positive or negative
+ * Return: Always 0 (Success)
  */
+
 int main(void)
 {
 int i;
+
 i = 0;
 positive_or_negative(i);
+
 return (0);
 }
