18c247c4faeb2cc893049ca078201d67eeab276b
n.bakenchich@gmail.com <n.bakenchich@gmail.com>
2023-03-30 16:02:53 -0700

    task 10 => program prints a[2] = 98

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+/**
+ * main - program prints a[2] = 98.
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 int n;
@@ -17,7 +21,7 @@ p = &n;
 * - you are not allowed to code anything else than this line of code
 */
 *(p + 5) = 98;
-/*  */
+/* ...so that this prints 98\n */
 printf("a[2] = %d\n", a[2]);
 return (0);
 }
