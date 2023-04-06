32eaf8e94d9205ba5f4ea6e79ca0e414b08a756d
n.bakenchich@gmail.com <n.bakenchich@gmail.com>
2023-04-05 18:26:27 -0700

    task 8 => Write a function that compares two strings and returns 1 if the strings can be considered identical, otherwise return 0.

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -29,7 +29,9 @@ return (check(s1, s2, i + 1, j) || check(s1, s2, i, j + 1));
 return (0);
 }
 /**
- * wildcmp - compares two strings and returns 1 if the strings can be considered identical, otherwise return 0.
+ * wildcmp - compares two strings
+ * and returns 1 if the strings can be considered identical,
+ * otherwise return 0.
  * @s1: string
  * @s2: string
  *
