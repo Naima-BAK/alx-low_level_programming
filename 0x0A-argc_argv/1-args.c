34c17ab3c2064d10e47a4f31edaefc41a4a12cb7
n.bakenchich@gmail.com <n.bakenchich@gmail.com>
2023-04-07 17:52:17 -0700

    task 0 => Write a program that prints its name, followed by a new line.

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+  * main - prints its name, followed by a new line.
+  * @argc : integer
+  * @argv : string
+  * Return: Always 0 (Success)
+  */
+
+int main(int argc, char *argv[])
+{
+printf("%c\n", agrv[0]);
+return (0);
+}
