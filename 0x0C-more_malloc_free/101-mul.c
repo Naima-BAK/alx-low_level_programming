2e3c16ce9f038c38a9a975d0d2149ddd1da3f2a1
n.bakenchich@gmail.com <n.bakenchich@gmail.com>
2023-04-13 18:36:59 -0700

    tasks

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -9,11 +9,9 @@
  *
  * Return: 0 or 1.
  */
-
 int is_digit(char *s)
 {
 int i = 0;
-
 while (s[i])
 {
 if (s[i] < '0' || s[i] > '9')
@@ -22,10 +20,8 @@ return (0);
 }
 i++;
 }
-
 return (1);
 }
-
 /**
  * _strlen - returns the length of a string
  *
@@ -36,25 +32,20 @@ return (1);
 int _strlen(char *s)
 {
 int size = 0;
-
 while (s[size] != '\0')
 {
 size++;
 }
-
 return (size);
 }
-
 /**
  * errors - return all errors and exit
  */
-
 void errors(void)
 {
-	printf("Error\n");
-	exit(98);
+printf("Error\n");
+exit(98);
 }
-
 /**
  * main - multiplies two positive numbers
  *
@@ -66,52 +57,43 @@ void errors(void)
 int main(int argc, char *argv[])
 {
 char *s1, *s2;
-int size1, size2, size;
-int i, j, a;
-int digit_1, digit_2, *multiplication;
-
+int size1, size2, size, i, j, a, digit_1, digit_2, *mul;
 a = 0;
 s1 = argv[1], s2 = argv[2];
-
 if (argc != 3 || !is_digit(s1) || !is_digit(s2))
 errors();
 size1 = _strlen(s1);
 size2 = _strlen(s2);
 size = size1 + size2 + 1;
-multiplication = malloc(sizeof(int) * size);
-if (!multiplication)
+mul = malloc(sizeof(int) * size);
+if (!mul)
 return (1);
 for (i = 0; i <= size1 + size2; i++)
-multiplication[i] = 0;
+mul[i] = 0;
 for (size1 = size1 - 1; size1 >= 0; size1--)
 {
 digit_1 = s1[size1] - '0';
 j = 0;
-
 for (size2 = _strlen(s2) - 1; size2 >= 0; size2--)
 {
 digit_2 = s2[size2] - '0';
-j += (digit_1 *digit_2) +multiplication[size1 + size2 + 1];
-multiplication[size1 + size2 + 1] = j % 10;
+j += (digit_1 *digit_2) +mul[size1 + size2 + 1];
+mul[size1 + size2 + 1] = j % 10;
 j /= 10;
 }
-
 if (j > 0)
-multiplication[size1 + size2 + 1] += j;
+mul[size1 + size2 + 1] += j;
 }
-
 for (i = 0; i < size - 1; i++)
 {
-if (multiplication[i])
+if (mul[i])
 a = 1;
 if (a)
-_putchar(multiplication[i] + '0');
+_putchar(mul[i] + '0');
 }
-
 if (!a)
 _putchar('0');
 _putchar('\n');
-
-free(multiplication);
+free(mul);
 return (0);
 }
