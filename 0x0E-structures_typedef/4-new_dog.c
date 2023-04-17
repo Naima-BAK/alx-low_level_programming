f26ba4efb55952acde3b433060689a123611d648
n.bakenchich@gmail.com <n.bakenchich@gmail.com>
2023-04-17 11:12:00 -0700

    task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,7 +1,9 @@
 #include <stdlib.h>
 #include "dog.h"
+
 /**
  * new_dog - creates a new dog
+ *
  * @name: name of dog
  * @age: age of dog
  * @owner: owner of dog
@@ -12,15 +14,18 @@ dog_t *new_dog(char *name, float age, char *owner)
 unsigned int name_size, owner_size, i;
 dog_t *dog;
 if (name == NULL || owner == NULL || age <= 0)
+{
 return (NULL);
+}
 dog = malloc(sizeof(dog_t));
 if (dog == NULL)
-return (NULL);
-for (name_size = 0; name[name_size]; name_size++)
 {
+return (NULL);
+}
+/** size of name of the dog*/
+for (name_size = 0; name[name_size]; name_size++)
 ;
 name_size++;
-}
 dog->name = malloc(name_size *sizeof(char));
 if (dog->name == NULL)
 {
@@ -28,24 +33,19 @@ free(dog);
 return (NULL);
 }
 for (i = 0; i < name_size; i++)
-{
 dog->name[i] = name[i];
-}
 dog->age = age;
 for (owner_size = 0; owner[owner_size]; owner_size++)
-{
 ;
 owner_size++;
-}
 dog->owner = malloc(owner_size *sizeof(char));
 if (dog->owner == NULL)
 {
+free(dog->name);
 free(dog);
 return (NULL);
 }
 for (i = 0; i < owner_size; i++)
-{
 dog->owner[i] = owner[i];
-}
 return (dog);
 }
