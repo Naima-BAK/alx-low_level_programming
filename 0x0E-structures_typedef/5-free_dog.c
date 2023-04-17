0a8ef7fad0c802ef43264582058b73f641a7c47f
n.bakenchich@gmail.com <n.bakenchich@gmail.com>
2023-04-17 11:24:27 -0700

    task 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - function that frees dogs
+ *
+ * @d: pointer to dog.
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+/* if d exist =>  free the dog*/
+if (d)
+{
+free(d->name);
+free(d->owner);
+free(d);
+}
+}
