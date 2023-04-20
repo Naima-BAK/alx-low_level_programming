fd4335f419a52f3a29dcc3ed30b328dd65b76b1c
n.bakenchich@gmail.com <n.bakenchich@gmail.com>
2023-04-19 18:29:29 -0700

    tasks

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,36 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints result of an operation
+ * @argc: integer
+ * @argv: string
+ * Return: integer
+ */
+
+int main(int argc, char *argv[])
+{
+int num1, num2, result;
+int (*operation_function)(int, int);
+
+if (argc != 4)
+{
+printf("Error\n");
+exit(98);
+}
+
+num1 = atoi(argv[1]);
+num2 = atoi(argv[3]);
+operation_function = get_op_func(argv[2]);
+
+if (!operation_function)
+{
+printf("Error\n");
+exit(98);
+}
+
+result = operation_function(num1, num2);
+printf("%d\n", result);
+return (0);
+}
