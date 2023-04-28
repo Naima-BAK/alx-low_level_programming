3d8e435b3152b78ece96f0d4474771e321cf5fc8
n.bakenchich@gmail.com <n.bakenchich@gmail.com>
2023-04-28 10:36:41 -0700

    task 2 fix

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,13 +11,14 @@ int i = 0;
 
 while (h)
 {
-if (h->str != NULL)
+if (h->str == NULL)
 {
-printf("[%d] %s\n", h->len, h->str);
+printf("[0] (nil)\n");
+/*printf("[%d] %s\n", h->len, h->str);*/
 }
 else
 {
-printf("[0] (nil)\n");
+printf("[%d] %s\n", h->len, h->str);
 }
 i++;
 h = h->next;
